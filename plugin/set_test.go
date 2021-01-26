8e1648006d9346fcc59bda6ec8f632a1383fb34a
Adrien Delorme <adrien.delorme@icloud.com>
2021-01-26 11:56:22 +0100

    update API version settings after merging main

--- plugin/set_test.go
+++ plugin/set_test.go
@@ -51,7 +51,7 @@ func TestSet(t *testing.T) {
 	if diff := cmp.Diff(SetDescription{
 		Version:        "1.1.1",
 		SDKVersion:     sdkVersion.String(),
-		APIVersion:     APIVersion,
+		APIVersion:     "x" + APIVersionMajor + "_" + APIVersionMinor,
 		Builders:       []string{"example", "example-2"},
 		PostProcessors: []string{"example", "example-2"},
 		Provisioners:   []string{"example", "example-2"},
