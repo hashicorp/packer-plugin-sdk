ba90450343af2a38d8cc2a12a6132c6fb2bc1045
Adrien Delorme <azr@users.noreply.github.com>
2021-04-13 16:35:27 +0200

    wrap up Test_RenderDocsFolder

--- cmd/packer-sdc/internal/renderdocs/renderdocs_test.go
+++ cmd/packer-sdc/internal/renderdocs/renderdocs_test.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"path/filepath"
 	"testing"
+
+	. "github.com/hashicorp/packer-plugin-sdk/cmd/packer-sdc/internal/cmd"
 )
 
 func Test_RenderDocsFolder(t *testing.T) {
 	tests := []struct {
 		cmd              []string
-		outputFolderHash map[string]string
+		outputFolderHash FileCheck
 		wantErr          bool
 	}{
 		{
@@ -18,16 +20,35 @@ func Test_RenderDocsFolder(t *testing.T) {
 				"-partials", filepath.Join("test-data/docs-partials"),
 				"-dst", filepath.Join("test-data/.docs"),
 			},
-			map[string]string{},
+			FileCheck{
+				ExpectedContent: map[string]string{
+					"test-data/.docs/builder-docs.mdx": `Hello and welcome to the awesome docs
+
+
+foo
+
+
+Bar:
+
+bar
+
+
+End of file
+`,
+				},
+			},
 			false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("%v", tt.cmd), func(t *testing.T) {
+			defer tt.outputFolderHash.Cleanup(t)
 			cmd := Command{}
 			if err := cmd.run(tt.cmd); (err != nil) != tt.wantErr {
 				t.Errorf("renderDocsFile() error = %v, wantErr %v", err, tt.wantErr)
 			}
+
+			tt.outputFolderHash.Verify(t, "")
 		})
 	}
 }
