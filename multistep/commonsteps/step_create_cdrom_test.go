ac048b4da78ae4e8ebc95d680da9954d6adef033
Saleem Rashid <dev@saleemrashid.com>
2021-05-12 13:53:49 +0100

    StepCreateCD: Clean up temporary directory and add more robust tests (#62)
    * StepCreateCD: Cleanup rootFolder
    * StepCreateCD: Check filenames and content during test
    * StepCreateCD: Add createFiles for more complex test cases
    * StepCreateCD: Test folder support
    * Update multistep/commonsteps/step_create_cdrom_test.go
    fix linting issues
    
    Co-authored-by: Adrien Delorme <azr@users.noreply.github.com>

--- multistep/commonsteps/step_create_cdrom_test.go
+++ multistep/commonsteps/step_create_cdrom_test.go
@@ -3,9 +3,10 @@ package commonsteps
 import (
 	"bytes"
 	"context"
+	"io/fs"
 	"io/ioutil"
 	"os"
-	"path"
+	"path/filepath"
 	"testing"
 
 	"github.com/hashicorp/packer-plugin-sdk/multistep"
@@ -29,6 +30,55 @@ func testStepCreateCDState(t *testing.T) multistep.StateBag {
 	return state
 }
 
+func createFiles(t *testing.T, rootFolder string, expected map[string]string) {
+	for fname, content := range expected {
+		path := filepath.Join(rootFolder, fname)
+		err := os.MkdirAll(filepath.Dir(path), 0777)
+		if err != nil {
+			t.Fatalf("mkdir -p: %s", err)
+		}
+		err = ioutil.WriteFile(path, []byte(content), 0666)
+		if err != nil {
+			t.Fatalf("writing file: %s", err)
+		}
+	}
+}
+
+func checkFiles(t *testing.T, rootFolder string, expected map[string]string) {
+	err := filepath.WalkDir(rootFolder, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			t.Fatalf("walking folder: %s", err)
+		}
+
+		if !d.IsDir() {
+			name, _ := filepath.Rel(rootFolder, path)
+
+			expectedContent, ok := expected[name]
+			if !ok {
+				t.Fatalf("unexpected file: %s", name)
+			}
+
+			content, err := ioutil.ReadFile(path)
+			if err != nil {
+				t.Fatalf("reading file: %s", err)
+			}
+			if string(content) != expectedContent {
+				t.Fatalf("unexpected content: %s", name)
+			}
+
+			delete(expected, name)
+		}
+
+		return nil
+	})
+	if err != nil {
+		t.Fatalf("WalkDir: %v", err)
+	}
+	if len(expected) != 0 {
+		t.Fatalf("missing files: %v", expected)
+	}
+}
+
 func TestStepCreateCD(t *testing.T) {
 	if os.Getenv("PACKER_ACC") == "" {
 		t.Skip("This test is only run with PACKER_ACC=1 due to the requirement of access to the disk management binaries.")
@@ -42,20 +92,22 @@ func TestStepCreateCD(t *testing.T) {
 	}
 	defer os.RemoveAll(dir)
 
-	files := make([]string, 3)
-
-	tempFileNames := []string{"test_cd_roms.tmp", "test cd files.tmp",
-		"Test-Test-Test5.tmp"}
-	for i, fname := range tempFileNames {
-		files[i] = path.Join(dir, fname)
-
-		_, err := os.Create(files[i])
-		if err != nil {
-			t.Fatalf("err: %s", err)
-		}
+	expected := map[string]string{
+		"test folder/b/test1": "1",
+		"test folder/b/test2": "2",
+		"test folder 2/x":     "3",
+		"test_cd_roms.tmp":    "4",
+		"test cd files.tmp":   "5",
+		"Test-Test-Test5.tmp": "6",
 	}
 
-	step.Files = files
+	createFiles(t, dir, expected)
+	files := []string{"test folder", "test folder 2/", "test_cd_roms.tmp", "test cd files.tmp", "Test-Test-Test5.tmp"}
+
+	step.Files = make([]string, len(files))
+	for i, fname := range files {
+		step.Files[i] = filepath.Join(dir, fname)
+	}
 	action := step.Run(context.Background(), state)
 
 	if err, ok := state.GetOk("error"); ok {
@@ -72,14 +124,15 @@ func TestStepCreateCD(t *testing.T) {
 		t.Fatalf("file not found: %s for %v", CD_path, step.Files)
 	}
 
-	if len(step.filesAdded) != 3 {
-		t.Fatalf("expected 3 files, found %d for %v", len(step.filesAdded), step.Files)
-	}
+	checkFiles(t, step.rootFolder, expected)
 
 	step.Cleanup(state)
 
 	if _, err := os.Stat(CD_path); err == nil {
 		t.Fatalf("file found: %s for %v", CD_path, step.Files)
+	}
+	if _, err := os.Stat(step.rootFolder); err == nil {
+		t.Fatalf("folder found: %s", step.rootFolder)
 	}
 }
 
@@ -96,8 +149,6 @@ func TestStepCreateCD_missing(t *testing.T) {
 	}
 	defer os.RemoveAll(dir)
 
-	expected := 0
-
 	step.Files = []string{"missing file.tmp"}
 	if action := step.Run(context.Background(), state); action != multistep.ActionHalt {
 		t.Fatalf("bad action: %#v for %v", action, step.Files)
@@ -113,7 +164,17 @@ func TestStepCreateCD_missing(t *testing.T) {
 		t.Fatalf("CD_path is not nil for %v", step.Files)
 	}
 
-	if len(step.filesAdded) != expected {
-		t.Fatalf("expected %d, found %d for %v", expected, len(step.filesAdded), step.Files)
+	checkFiles(t, step.rootFolder, nil)
+
+	step.Cleanup(state)
+
+	if _, err := os.Stat(step.rootFolder); err == nil {
+		t.Fatalf("folder found: %s", step.rootFolder)
+	}
+
+	step.Cleanup(state)
+
+	if _, err := os.Stat(step.rootFolder); err == nil {
+		t.Fatalf("folder found: %s", step.rootFolder)
 	}
 }
