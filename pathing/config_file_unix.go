d4e0efff050584853f0c9725ff454fc023b88280
John Troy <jtroy@galileosuite.com>
2024-09-06 14:59:08 -0400

    Add AIX support for cache/config dirs

--- pathing/config_file_unix.go
+++ pathing/config_file_unix.go
@@ -1,7 +1,7 @@
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: MPL-2.0
 
-//go:build darwin || freebsd || linux || netbsd || openbsd || solaris
+//go:build aix || darwin || freebsd || linux || netbsd || openbsd || solaris
 
 package pathing
 
