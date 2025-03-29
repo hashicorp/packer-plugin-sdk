fd246168dd8c4a4a0a076cdbfbc2caf8b3285ec4
anshul sharma <anshul.sharma@hashicorp.com>
2025-03-29 18:13:29 +0530

    unit test case

--- sdk-internals/communicator/ssh/windows_named_pipe_test.go
+++ sdk-internals/communicator/ssh/windows_named_pipe_test.go
@@ -7,12 +7,12 @@ package ssh
 
 import "testing"
 
-func TestGetSSHAgentConnection(t *testing.T) {
+func TestGetSSHAgentConnection_NoPipe(t *testing.T) {
 	conn, err := GetSSHAgentConnection()
-	if err != nil {
-		t.Fatal("Expected nil error when named pipe exist, got non-nil")
+	if err == nil {
+		t.Fatal("Expected error when named pipe does not exist, got nil")
 	}
-	if conn == nil {
-		t.Fatal("Expected connection, got nil")
+	if conn != nil {
+		t.Fatal("Expected nil connection, got non-nil")
 	}
 }
