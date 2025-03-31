d037325f70ad5d1696c7dc78546bf26904b404ad
Lucas Bajolet <lucas.bajolet@hashicorp.com>
2025-03-31 09:40:12 -0400

    version: cut release v0.6.1

--- version/version.go
+++ version/version.go
@@ -20,7 +20,7 @@ var Version = "0.6.1"
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
 // such as "dev" (in development), "beta", "rc1", etc.
-var VersionPrerelease = "dev"
+var VersionPrerelease = ""
 
 // The metadata for the version, this is optional information to add around
 // a particular release.
